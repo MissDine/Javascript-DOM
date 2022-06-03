0e2b1f793188ad184b71ae9fe5750b8ada7bb233
MissDine <enidnyatichi1212@gmail.com>
2022-06-03 16:51:10 +0300

    Feat:Edit

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 // The DOM-Creat
-const para = document.querySelector(".error")
+// Getting access to a paragrapgh ot a tag element
+const para = document.querySelector("p")
 console.log(para);
+// Getting access to a class element
+const Par = document.querySelector(".error")
+console.log(Par);
+
+
