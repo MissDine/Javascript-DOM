bc0b903ee557cb78c6b98eeb81b781d1cafd83ec
MissDine <enidnyatichi1212@gmail.com>
2022-06-03 16:57:21 +0300

    Feat:Edit

--- index.js
+++ index.js
@@ -9,5 +9,8 @@ console.log(Par);
 // Getting access to a div with a class name
 const Pa = document.querySelector("div.cal")
 console.log(Pa);
+// For uniques selectors - go to the item on the browser and copy its selector on the console eg
+const Ba = document.querySelector("body > h1")
+console.log(Ba);
 
 
